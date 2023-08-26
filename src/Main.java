1bf5c338a414a2689e2ab4fda4da468a4c34616d
Manasaravar <manasaravar@gmail.com>
2023-08-26 15:38:18 +0300

    Hello

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\t\t\t Hello \"world!\"\n");
+        System.out.print("C:\\Windows\\System32\\\n");
+        System.out.print("Привет!");
+    }
+}
